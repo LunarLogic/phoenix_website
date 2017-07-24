6b09e705fcc1e3130ccd4d68e8cf6439097a4bba
ArturT <artur.trzop@lunarlogic.io>
2017-07-24 13:23:29 +0200

    Add distillery to deps

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule PhoenixWebsite.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:distillery, "~> 1.4", runtime: false},
     ]
   end
 
